cb1d37fd2e25c426b7ab3ac8e854928d50edb9a3
塩澤 <shiozawa@shio-mac-mini.local>
2017-03-31 11:52:45 +0900

    Initial Commit

---
+++ SwitchContainer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwitchContainer
+//
+//  Created by 塩澤 on 2017/03/31.
+//  Copyright © 2017年 co.i-sac.test. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
